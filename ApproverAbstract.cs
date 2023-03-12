d26e383b17573d54f2c5b328e7924e19640f090e
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-03-12 21:38:17 +0100

    approver abstract

---
+++ ApproverAbstract.cs
@@ -0,0 +1,26 @@
+﻿namespace chain_of_responsibility_pattern
+{
+    public class ApproverAbstract : IApproverHandler
+    {
+        private IApproverHandler _nextHandler;
+
+        public virtual object Handle(object request)
+        {
+            if (_nextHandler is not null)
+            {
+                return _nextHandler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public IApproverHandler SetNextApprover(IApproverHandler handler)
+        {
+            _nextHandler = handler;
+
+            return handler;
+        }
+    }
+}
