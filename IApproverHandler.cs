a82a6fa45a7ebf083c219732174c5c8f86185905
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-03-12 21:26:05 +0100

    IApproverHandler

---
+++ IApproverHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace chain_of_responsibility_pattern
+{
+    public interface IApproverHandler
+    {
+        object Handle(object request);
+        IApproverHandler SetNextApprover(IApproverHandler handler);
+    }
+}
