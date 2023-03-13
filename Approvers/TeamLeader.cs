e5bf9311b7842e820022dea00d9c3530f1f26ef2
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-03-13 20:52:23 +0100

    Approvers => team leader

---
+++ Approvers/TeamLeader.cs
@@ -0,0 +1,19 @@
+﻿namespace chain_of_responsibility_pattern.Approvers
+{
+    public class TeamLeader : ApproverAbstract
+    {
+        public override object Handle(object request)
+        {
+            if (request is not null)
+            {
+                // do the job and return objecy
+                return request;
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+
+        }
+    }
+}
