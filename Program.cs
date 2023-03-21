d64b3537b1596301cbcd203933b8d52e8447c208
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-03-21 22:30:43 +0100

    using approve process

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using chain_of_responsibility_pattern;
+using chain_of_responsibility_pattern.Approvers;
+
 Console.WriteLine("Hello, World!");
+
+
+ApproverAbstract teamLead = new TeamLeader();
+ApproverAbstract director = new Director();
+ApproverAbstract boss = new Boss();
+
+teamLead.SetNextApprover(director);
+director.SetNextApprover(boss);
